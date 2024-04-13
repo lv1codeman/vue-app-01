b003eb5a2395ed7b19eb0aa45fffd5ebed551aec
lv1codeman <lv1codeman@gmail.com>
2024-04-14 04:50:19 +0800

    已可上傳資料，資料驗證未實作

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
           :key="index"
         >
           <template v-if="key === 'curriAgent'">
-            <!-- Check if it's the third item -->
+            <!-- 顯示課務組承辦人下拉選單 -->
             <label
               class="text-gray-700 text-sm font-bold mr-2 flex-shrink-0 md:w-1/4 text-right max-[768px]:text-left"
               :for="key"
@@ -23,13 +23,14 @@
               class="shadow border-2 border-indigo-200 focus:border-indigo-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline md:w-3/4"
               :id="key"
               v-model="selectedOption"
+              @change="curriAgentOnChange($event)"
             >
               <option
                 v-for="(option, index) in curriAgentOptions"
                 :key="index"
                 :value="option.value"
               >
-                {{ option.label }}
+                {{ option.name }}
               </option>
             </select>
           </template>
@@ -55,11 +56,11 @@
         </div>
 
         <!-- 以下按鈕 -->
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-around">
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
-            @click="add"
+            @click="btnSubmitClick"
           >
             登記
           </button>
@@ -98,6 +99,13 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export default {
   created() {},
+  mounted() {
+    document.getElementById("curriAgentExt").placeholder = "請選擇課務組承辦人";
+    document.getElementById("curriAgentEmail").placeholder =
+      "請選擇課務組承辦人";
+    document.getElementById("curriAgentExt").disabled = true;
+    document.getElementById("curriAgentEmail").disabled = true;
+  },
   data() {
     return {
       dataFields: {
@@ -114,22 +122,50 @@ export default {
       },
       selectedOption: "", // Initially empty
       curriAgentOptions: [
-        { label: "鍾博凱", value: 1, disabled: false },
-        { label: "黃思嘉", value: 2, disabled: false },
-        { label: "黃嘉玲", value: 3, disabled: false },
-        { label: "范豈瑗", value: 4, disabled: false },
+        {
+          name: "鍾博凱",
+          value: 0,
+          disabled: false,
+          ext: "#5626",
+          email: "s26153@cc.ncue.edu.tw",
+        },
+        {
+          name: "黃思嘉",
+          value: 1,
+          disabled: false,
+          ext: "#5622",
+          email: "loria@cc.ncue.edu.tw",
+        },
+        {
+          name: "黃嘉玲",
+          value: 2,
+          disabled: false,
+          ext: "#5623",
+          email: "chialing@cc.ncue.edu.tw",
+        },
+        {
+          name: "范豈瑗",
+          value: 3,
+          disabled: false,
+          ext: "#5624",
+          email: "ang3188@cc.ncue.edu.tw",
+        },
       ],
     };
   },
   computed: {},
   methods: {
-    add() {
-      addAgent();
+    curriAgentOnChange() {
+      document.getElementById("curriAgentExt").value =
+        this.curriAgentOptions[this.selectedOption].ext;
+      document.getElementById("curriAgentEmail").value =
+        this.curriAgentOptions[this.selectedOption].email;
     },
     async show() {
       gogo(db)
         .then((users) => {
           console.log("取得使用者資料成功：", users);
+          console.log("取得使用者資料成功：", users.length);
         })
         .catch((error) => {
           console.error("取得使用者資料失敗：", error);
@@ -137,9 +173,10 @@ export default {
     },
     getInputs() {
       const inputdata = [];
-      console.log(this.$refs.inputs);
-      console.log(typeof this.$refs.inputs[0].value);
+      // console.log(this.$refs.inputs);
+      // console.log(typeof this.$refs.inputs[0].value);
       for (let i = 0; i < this.$refs.inputs.length; i++) {
+        // console.log(this.$refs.inputs[i]);
         console.log(this.$refs.inputs[i].value);
         inputdata.push(this.$refs.inputs[i].value);
       }
@@ -155,20 +192,44 @@ export default {
 
     btnSubmitClick() {
       const res = this.getInputs();
+      console.log("使用者輸入: " + res);
+      const obj = {
+        college: res[0],
+        collegeFullName: res[1],
+        dept: res[2],
+        deptFullName: res[3],
+        agent: res[4],
+        agentExt: "#" + res[5],
+        agentEmail: res[6],
+        curriAgent: this.curriAgentOptions[this.selectedOption].name,
+        curriAgentExt: res[7],
+        curriAgentEmail: res[8],
+      };
+      console.log("上傳前的資料：", obj);
       Swal.fire({
         title: "確認上傳資料",
-        text: res,
+        text: res + "," + this.curriAgentOptions[this.selectedOption].name,
         showDenyButton: true,
         showCancelButton: true,
         confirmButtonText: "送出",
         cancelButtonText: "返回編輯",
         denyButtonText: `取消並清除`,
       }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
+          // 取得目前資料庫的user數，將其+1作為id給新的user
+          gogo(db)
+            .then((users) => {
+              console.log("取得使用者資料成功：", users);
+              console.log("取得使用者資料成功：", users.length.toString());
+              addAgent(obj, (users.length + 1).toString());
+            })
+            .catch((error) => {
+              console.error("取得使用者資料失敗：", error);
+            });
+
           Swal.fire("資料已送出", "", "success");
         } else if (result.isDenied) {
-          // Swal.fire("欄位已重置", "", "info");
+          Swal.fire("欄位已重置", "", "info");
           this.clearInputs();
         }
       });
--- src/assets/js/setAgent.js
+++ src/assets/js/setAgent.js
@@ -7,38 +7,39 @@ const db = getFirestore(app);
 
 import { doc, setDoc } from "firebase/firestore";
 
-export async function addAgent() {
+export async function addAgent(agent, id) {
   console.log("addAgent start...");
+  console.log(agent);
   await setDoc(
-    doc(db, "agents", "3"),
-    // {
-    //   id: agent.id,
-    //   dept: agent.dept,
-    //   college: agent.college,
-    //   collegeFullName: agent.collegeFullName,
-    //   dept: agent.dept,
-    //   deptFullName: agent.deptFullName,
-    //   agent: agent.agent,
-    //   agentExt: agent.agentExt,
-    //   agentEmail: agent.agentEmail,
-    //   curriAgent: agent.curriAgent,
-    //   curriAgentExt: agent.curriAgentExt,
-    //   curriAgentEmail: agent.curriAgentEmail,
-    // },
+    doc(db, "agents", id),
     {
-      id: 333,
-      dept: 333,
-      college: 333,
-      collegeFullName: 333,
-      dept: 333,
-      deptFullName: 333,
-      agent: 333,
-      agentExt: 333,
-      agentEmail: 333,
-      curriAgent: 333,
-      curriAgentExt: 333,
-      curriAgentEmail: 333,
+      // id: agent.id,
+      dept: agent.dept,
+      college: agent.college,
+      collegeFullName: agent.collegeFullName,
+      dept: agent.dept,
+      deptFullName: agent.deptFullName,
+      agent: agent.agent,
+      agentExt: agent.agentExt,
+      agentEmail: agent.agentEmail,
+      curriAgent: agent.curriAgent,
+      curriAgentExt: agent.curriAgentExt,
+      curriAgentEmail: agent.curriAgentEmail,
     },
+    // {
+    //   id: 333,
+    //   dept: 333,
+    //   college: 333,
+    //   collegeFullName: 333,
+    //   dept: 333,
+    //   deptFullName: 333,
+    //   agent: 333,
+    //   agentExt: 333,
+    //   agentEmail: 333,
+    //   curriAgent: 333,
+    //   curriAgentExt: 333,
+    //   curriAgentEmail: 333,
+    // },
     { merge: true }
   );
   return "add success";
