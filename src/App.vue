9b774248280baa86d7fbda38cd8e63a8c62485d5
lv1codeman <lv1codeman@gmail.com>
2024-04-14 22:24:53 +0800

    delete unused code

--- src/App.vue
+++ src/App.vue
@@ -138,18 +138,6 @@ export default {
         curriAgentEmail: { text: "尚未輸入", isShow: false, isCorrect: false },
       },
       showValidation: {}, // Keep track of input touched status
-      validationResults: {
-        college: "尚未輸入",
-        collegeFullName: "尚未輸入",
-        dept: "尚未輸入",
-        deptFullName: "尚未輸入",
-        agent: "尚未輸入",
-        agentExt: "尚未輸入",
-        agentEmail: "尚未輸入",
-        curriAgent: "尚未輸入",
-        curriAgentExt: "尚未輸入",
-        curriAgentEmail: "尚未輸入",
-      },
       dataFields: {
         college: "學院",
         collegeFullName: "學院全名",
@@ -199,59 +187,52 @@ export default {
   methods: {
     validateInput(event, key) {
       // 如果這個input有輸入值才顯示validation
-      // this.showValidation[key] = event.target.value === "" ? false : true;
       this.validations[key].isShow = event.target.value === "" ? false : true;
-      // var str = key;
-      // console.log();
+
       const chineseRegex = /^[\u4E00-\u9FA5]+$/;
+      const extRegex = /^\d{4}$/;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-      // const chineseRegex = /\p{Unified_Ideograph}/u;
-
-      console.log(chineseRegex.test(event.target.value));
-      if (chineseRegex.test(event.target.value)) {
-        switch (key) {
-          case "college":
-            this.validations[key].isCorrect = true;
-            break;
-          case "collegeFullName":
-            this.validationResults.collegeFullName = "限輸入文字";
-          case "dept":
-            this.validationResults.dept = "限輸入文字";
-          case "deptFullName":
-            this.validationResults.deptFullName = "限輸入文字";
-          case "agent":
-            this.validationResults.agent = "限輸入文字";
-          case "agentExt":
-            this.validationResults.agentExt = "限輸入數字";
-          case "agentEmail":
-            this.validationResults.agentEmail = "需輸入email格式";
+      const needChineseCheck = [
+        "college",
+        "collegeFullName",
+        "dept",
+        "deptFullName",
+        "agent",
+      ];
+      if (needChineseCheck.includes(key)) {
+        if (chineseRegex.test(event.target.value))
+          this.validations[key].isCorrect = true;
+        else {
+          this.validations[key].isCorrect = false;
+          this.validations[key].text = "格式錯誤，限輸入中文";
         }
-      } else {
-        switch (key) {
-          case "college":
-            this.validations[key].isCorrect = false;
-            this.validations[key].text = "格式錯誤，限輸入中文";
-            break;
-          case "collegeFullName":
-            this.validationResults.collegeFullName = "限輸入文字";
-          case "dept":
-            this.validationResults.dept = "限輸入文字";
-          case "deptFullName":
-            this.validationResults.deptFullName = "限輸入文字";
-          case "agent":
-            this.validationResults.agent = "限輸入文字";
-          case "agentExt":
-            this.validationResults.agentExt = "限輸入數字";
-          case "agentEmail":
-            this.validationResults.agentEmail = "需輸入email格式";
+      } else if (key.includes("agentExt")) {
+        if (extRegex.test(event.target.value))
+          this.validations[key].isCorrect = true;
+        else {
+          this.validations[key].isCorrect = false;
+          this.validations[key].text = "格式錯誤，限輸入4位數數字";
+        }
+      } else if (key.includes("agentEmail")) {
+        if (emailRegex.test(event.target.value))
+          this.validations[key].isCorrect = true;
+        else {
+          this.validations[key].isCorrect = false;
+          this.validations[key].text = "email格式錯誤，範例：example@gmail.com";
         }
       }
     },
     curriAgentOnChange() {
+      // 只要下拉選單有change，代表已經選了其中一位課務組承辦人，將自動帶出其他相關欄位的值
       document.getElementById("curriAgentExt").value =
         this.curriAgentOptions[this.selectedOption].ext;
       document.getElementById("curriAgentEmail").value =
         this.curriAgentOptions[this.selectedOption].email;
+      // 因為值被自動帶出，所以內容必定正確
+      this.validations["curriAgent"].isCorrect = true;
+      this.validations["curriAgentExt"].isCorrect = true;
+      this.validations["curriAgentEmail"].isCorrect = true;
     },
     async show() {
       getAllAgents(db)
@@ -278,54 +259,74 @@ export default {
         this.$refs.inputs[i].value = "";
       }
       this.selectedOption = "";
-      Object.keys(this.showValidation).forEach(
-        (v) => (this.showValidation[v] = false)
+      Object.keys(this.validations).forEach(
+        (v) => (
+          (this.validations[v].isShow = false),
+          (this.validations[v].isCorrect = false)
+        )
       );
     },
 
     btnSubmitClick() {
       const res = this.getInputs();
-      console.log("使用者輸入: " + res);
-      const obj = {
-        college: res[0],
-        collegeFullName: res[1],
-        dept: res[2],
-        deptFullName: res[3],
-        agent: res[4],
-        agentExt: "#" + res[5],
-        agentEmail: res[6],
-        curriAgent: this.curriAgentOptions[this.selectedOption].name,
-        curriAgentExt: res[7],
-        curriAgentEmail: res[8],
-      };
-      console.log("上傳前的資料：", obj);
-      Swal.fire({
-        title: "確認上傳資料",
-        text: res + "," + this.curriAgentOptions[this.selectedOption].name,
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: "送出",
-        cancelButtonText: "返回編輯",
-        denyButtonText: `取消並清除`,
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // 取得目前資料庫的user數，將其+1作為id給新的user
-          getAllAgents(db)
-            .then((users) => {
-              console.log("取得使用者資料成功：", users);
-              console.log("取得使用者資料成功：", users.length.toString());
-              addAgent(db, obj, (users.length + 1).toString());
-            })
-            .catch((error) => {
-              console.error("取得使用者資料失敗：", error);
-            });
 
-          Swal.fire("資料已送出", "", "success");
-        } else if (result.isDenied) {
-          Swal.fire("欄位已重置", "", "info");
-          this.clearInputs();
+      const allCorrect = Object.values(this.validations).every(
+        ({ isCorrect }) => isCorrect
+      );
+      console.log("allCorrect=" + allCorrect);
+
+      const isCorrectValues = {};
+      for (const key in this.validations) {
+        if (this.validations.hasOwnProperty(key)) {
+          isCorrectValues[key] = this.validations[key].isCorrect;
         }
-      });
+      }
+      console.log("isCorrectValues", isCorrectValues);
+
+      if (allCorrect) {
+        const obj = {
+          college: res[0],
+          collegeFullName: res[1],
+          dept: res[2],
+          deptFullName: res[3],
+          agent: res[4],
+          agentExt: "#" + res[5],
+          agentEmail: res[6],
+          curriAgent: this.curriAgentOptions[this.selectedOption].name,
+          curriAgentExt: res[7],
+          curriAgentEmail: res[8],
+        };
+        console.log("上傳前的資料：", obj);
+        Swal.fire({
+          title: "確認上傳資料",
+          text: res + "," + this.curriAgentOptions[this.selectedOption].name,
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: "送出",
+          cancelButtonText: "返回編輯",
+          denyButtonText: `取消並清除`,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // 取得目前資料庫的user數，將其+1作為id給新的user
+            getAllAgents(db)
+              .then((users) => {
+                console.log("取得使用者資料成功：", users);
+                console.log("取得使用者資料成功：", users.length.toString());
+                addAgent(db, obj, (users.length + 1).toString());
+              })
+              .catch((error) => {
+                console.error("取得使用者資料失敗：", error);
+              });
+
+            Swal.fire("資料已送出", "", "success");
+          } else if (result.isDenied) {
+            Swal.fire("欄位已重置", "", "info");
+            this.clearInputs();
+          }
+        });
+      } else {
+        Swal.fire("資料有誤", "請重新檢查", "error");
+      }
     },
   },
 };
